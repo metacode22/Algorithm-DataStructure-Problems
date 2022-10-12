e11f4a24ead8489c961dd43609cad32490f40f62
metacode22 <alohajune22@gmail.com>
2022-10-12 13:43:52 +0900

    feat: 2022.10.12

--- JavaScript/practiceJavaScript.js
+++ JavaScript/practiceJavaScript.js
@@ -1,31 +1,26 @@
-function divide(array, left, right, pivot) {
-    while (left <= right) {
-        while (array[left] < pivot) left++; // pivot보다 크거나 같은 것 찾기
-        while (array[right] > pivot) right--; // pivot보다 작거나 같은 것 찾기
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-        // 엇갈리지 않았다면 swap, 맨 위의 while문을 지켜줘야 한다. 위 2개의 while문에 의해 left가 right보다 커졌을 수도 있다.
-        if (left <= right) {
-            [array[left], array[right]] = [array[right], array[left]];
-            left++;
-            right--;
-        }
-    }
+const inputString = input[0];
+const m = Number(input[1]);
+const inputCommands = input.slice(2);
 
-    return left;
-}
+const solution = (inputString, inputCommands) => {
+    const leftStack = inputString.split('');
+    const rightStack = [];
 
-function quickSort(array, left = 0, right = array.length - 1) {
-    if (left >= right) return;
+    inputCommands.forEach(inputCommand => {
+        const [command, stringToAdd] = inputCommand.split(' ');
 
-    const mid = Math.floor((left + right) / 2);
-    const pivot = array[mid];
-    const partition = divide(array, left, right, pivot);
+        if (command === 'L' && leftStack.length !== 0) rightStack.push(leftStack.pop());
+        if (command === 'D' && rightStack.length !== 0) leftStack.push(rightStack.pop());
+        if (command === 'B' && leftStack.length !== 0) leftStack.pop();
+        if (command === 'P') leftStack.push(stringToAdd);
+    });
 
-    quickSort(array, left, partition - 1);
-    quickSort(array, partition, right);
+    return leftStack.concat(rightStack.reverse()).join('');
+};
 
-    return array;
-}
-
-const sorted = quickSort([12, 2, 7, 4, 5, 3]);
-console.log(sorted);
+const result = solution(inputString, inputCommands);
+console.log(result);
--- JavaScript/practiceJavaScript2.js
+++ JavaScript/practiceJavaScript2.js
@@ -1,1 +1,3 @@
-console.log([1] === [2]);
+const temp = [1, 2, 3, 4];
+temp.splice(1, 1, 0);
+console.log(temp);
