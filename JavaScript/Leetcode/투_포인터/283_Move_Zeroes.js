37bef9dd055e861f1f08e31790ad50ab359288a7
metacode22 <alohajune22@gmail.com>
2022-12-30 12:55:06 +0900

    리트코드 283 Move Zeores 재풀이

--- JavaScript/Leetcode/투_포인터/283_Move_Zeroes.js
+++ JavaScript/Leetcode/투_포인터/283_Move_Zeroes.js
@@ -1,3 +1,28 @@
+// 2022.12.30.
+var moveZeroes = function (nums) {
+  if (nums.length === 1) {
+    return nums;
+  }
+
+  let left = 0;
+  let right = 1;
+
+  while (right < nums.length) {
+    if (nums[left] === 0 && nums[right] !== 0) {
+      [nums[left], nums[right]] = [nums[right], nums[left]];
+      left += 1;
+      right += 1;
+    } else if (nums[left] !== 0 && nums[right] === 0) {
+      left = right;
+      right += 1;
+    } else {
+      right += 1;
+    }
+  }
+
+  return nums;
+};
+
 var moveZeroes = function (nums) {
   if (nums.length < 2) return;
 
