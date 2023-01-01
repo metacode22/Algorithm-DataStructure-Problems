b66133a98885bcdf946e39e39c3367d54345b637
metacode22 <alohajune22@gmail.com>
2023-01-02 01:10:37 +0900

    프로그래머스 같은 숫자는 싫어 재풀이

--- JavaScript/Programmers/level_1/같은_숫자는_싫어.js
+++ JavaScript/Programmers/level_1/같은_숫자는_싫어.js
@@ -1,19 +1,32 @@
+// 2023.01.02
 function solution(arr) {
   const stack = [];
-  const result = [];
 
-  arr.forEach(number => {
-    if (stack[0] !== number) {
-      stack.pop();
+  arr.forEach((value, index) => {
+    if (index === 0 || value !== stack[stack.length - 1]) {
+      stack.push(value);
     }
-
-    if (!stack.length) {
-      stack.push(number);
-      result.push(number);
-    }
-
-    if (stack[0] === number) return;
   });
 
-  return result;
+  return stack;
 }
+
+// function solution(arr) {
+//   const stack = [];
+//   const result = [];
+
+//   arr.forEach(number => {
+//     if (stack[0] !== number) {
+//       stack.pop();
+//     }
+
+//     if (!stack.length) {
+//       stack.push(number);
+//       result.push(number);
+//     }
+
+//     if (stack[0] === number) return;
+//   });
+
+//   return result;
+// }
