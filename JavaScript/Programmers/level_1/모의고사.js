31fc7fe62e9721a7bae56b3db194f0739df0fdee
metacode22 <alohajune22@gmail.com>
2023-01-04 01:03:33 +0900

    프로그래머스 모의고사 재풀이

--- JavaScript/Programmers/level_1/모의고사.js
+++ JavaScript/Programmers/level_1/모의고사.js
@@ -1,3 +1,30 @@
+// 2023.01.03.
+function solution(answers) {
+  const one = [1, 2, 3, 4, 5];
+  const two = [2, 1, 2, 3, 2, 4, 2, 5];
+  const three = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  let oneCount = 0;
+  let twoCount = 0;
+  let threeCount = 0;
+
+  answers.forEach((answer, index) => {
+    if (answer === one[index % one.length]) oneCount += 1;
+    if (answer === two[index % two.length]) twoCount += 1;
+    if (answer === three[index % three.length]) threeCount += 1;
+  });
+
+  const result = [];
+  const maxCount = Math.max(oneCount, twoCount, threeCount);
+
+  if (maxCount) {
+    if (maxCount === oneCount) result.push(1);
+    if (maxCount === twoCount) result.push(2);
+    if (maxCount === threeCount) result.push(3);
+  }
+
+  return result;
+}
+
 function check(person, answers) {
   let result = 0;
 
--- practice.js
+++ practice.js
@@ -1,1 +1,27 @@
-const temp = [1, 2, 3];
+const employees = [
+  {
+    firstName: 'John',
+    lastName: 'Doe',
+  },
+  {
+    firstName: 'David',
+    lastName: 'Hue',
+  },
+  {
+    fistName: 'Jack',
+    lastName: 'Daniel',
+  },
+];
+console.log(
+  Object.assign(
+    {},
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+    },
+    {
+      fistName: 'Jack',
+      lastName: 'Daniel',
+    },
+  ),
+);
