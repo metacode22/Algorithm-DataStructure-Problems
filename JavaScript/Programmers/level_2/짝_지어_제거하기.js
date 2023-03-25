4dca2431d55b3bb2ad48b8ea198c57b7eb155d34
metacode22 <alohajune22@gmail.com>
2023-03-25 13:34:16 +0900

    프로그래머스 짝지어 제거하기 재풀이

--- JavaScript/Programmers/level_2/짝_지어_제거하기.js
+++ JavaScript/Programmers/level_2/짝_지어_제거하기.js
@@ -1,3 +1,18 @@
+// 2023-03-25
+const solution = alphabets => {
+  const stack = [];
+
+  alphabets.split('').forEach(alphabet => {
+    if (stack.length && stack[stack.length - 1] === alphabet) {
+      stack.pop();
+    } else {
+      stack.push(alphabet);
+    }
+  });
+
+  return stack.length === 0 ? 1 : 0;
+};
+
 function solution(s) {
   const stack = [];
 
