0b93b82da008250597826326072375d2a0a98180
metacode22 <alohajune22@gmail.com>
2023-03-27 18:39:18 +0900

    리트코드 3Sum 재풀이

--- JavaScript/Leetcode/15_3Sum.js
+++ JavaScript/Leetcode/15_3Sum.js
@@ -1,25 +1,56 @@
+// 2023-03-27
 const threeSum = nums => {
-  const sortedNums = nums.sort((a, b) => a - b);
   const result = [];
+  nums.sort((a, b) => a - b);
 
-  for (let i = 0; i < sortedNums.length - 2; i++) {
-    if (nums[i] === nums[i - 1]) continue;
-    if (nums[i] > 0) break;
+  nums.forEach((num, index) => {
+    if (index > 0 && num === nums[index - 1]) return;
 
-    for (let j = i + 1; j < sortedNums.length - 1; j++) {
-      if (j > i + 1 && nums[j] === nums[j - 1]) continue;
-      if (nums[i] + nums[j] > 0) break;
+    let left = index + 1;
+    let right = nums.length - 1;
 
-      for (let k = j + 1; k < sortedNums.length; k++) {
-        if (k > j + 1 && nums[k] === nums[k - 1]) continue;
-        if (nums[i] + nums[j] + nums[k] > 0) break;
+    while (left < right) {
+      const sum = num + nums[left] + nums[right];
 
-        if (nums[i] + nums[j] + nums[k] === 0) {
-          result.push([nums[i], nums[j], nums[k]]);
+      if (sum === 0) {
+        result.push([num, nums[left], nums[right]]);
+        left += 1;
+        while (nums[left] === nums[left - 1]) {
+          left += 1;
         }
       }
+
+      if (sum < 0) left += 1;
+      if (sum > 0) right -= 1;
     }
-  }
+  });
 
   return result;
 };
+
+// 2023-03-26
+// const threeSum = nums => {
+//   const sortedNums = nums.sort((a, b) => a - b);
+//   const result = [];
+
+//   for (let i = 0; i < sortedNums.length - 2; i++) {
+//     if (nums[i] === nums[i - 1]) continue;
+//     if (nums[i] > 0) break;
+
+//     for (let j = i + 1; j < sortedNums.length - 1; j++) {
+//       if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+//       if (nums[i] + nums[j] > 0) break;
+
+//       for (let k = j + 1; k < sortedNums.length; k++) {
+//         if (k > j + 1 && nums[k] === nums[k - 1]) continue;
+//         if (nums[i] + nums[j] + nums[k] > 0) break;
+
+//         if (nums[i] + nums[j] + nums[k] === 0) {
+//           result.push([nums[i], nums[j], nums[k]]);
+//         }
+//       }
+//     }
+//   }
+
+//   return result;
+// };
