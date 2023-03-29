7b1d15fdfe8d200521d6c26d8c342a3a07dd68fa
metacode22 <alohajune22@gmail.com>
2023-03-29 09:26:48 +0900

    리트코드 Best Time to Buy and Sell Stock 재풀이

--- JavaScript/Leetcode/121_Best_Time_to_Buy_and_Sell_Stock.js
+++ JavaScript/Leetcode/121_Best_Time_to_Buy_and_Sell_Stock.js
@@ -1,3 +1,32 @@
+// 2023-03-29
+const maxProfit = prices => {
+  const result = [];
+  let min = Infinity;
+  let currentValue = 0;
+  let currentSum = 0;
+
+  prices.forEach((price, index) => {
+    if (price < min) {
+      min = price;
+      result.push(currentSum);
+      currentSum = 0;
+      currentValue = min;
+    } else {
+      if (currentValue < price) {
+        currentSum += price - currentValue;
+        currentValue = price;
+      }
+    }
+
+    if (index === prices.length - 1) {
+      result.push(currentSum);
+    }
+  });
+
+  return Math.max(...result);
+};
+
+// 2022-12-28
 var maxProfit = function (prices) {
   let left = 0;
   let right = 1;
@@ -23,7 +52,7 @@ var maxProfit = function (prices) {
 };
 
 // 2023-02-18
-var maxProfit = function(prices) {
+var maxProfit = function (prices) {
   const result = [0];
   let smallestPrice = prices[0];
   let currentIndex = 1;
@@ -31,17 +60,17 @@ var maxProfit = function(prices) {
   let count = 0;
 
   prices.forEach(price => {
-      if (price < smallestPrice) {
-          smallestPrice = price;
-          result[currentIndex] = count;
-          count = 0;
-          currentIndex += 1;
-          currentPrice = price;
-      } else if (currentPrice < price) {
-          count += price - currentPrice;
-          currentPrice = price;
-      }
-  })
+    if (price < smallestPrice) {
+      smallestPrice = price;
+      result[currentIndex] = count;
+      count = 0;
+      currentIndex += 1;
+      currentPrice = price;
+    } else if (currentPrice < price) {
+      count += price - currentPrice;
+      currentPrice = price;
+    }
+  });
 
   result[currentIndex] = count;
 
