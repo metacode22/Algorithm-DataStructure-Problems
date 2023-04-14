da63efcb44bc50f4d0dacc615e61e6e9e4eb0290
metacode22 <alohajune22@gmail.com>
2023-04-14 16:43:17 +0900

    프로그래머스 구명보트 재풀이

--- JavaScript/Programmers/level_2/구명보트.js
+++ JavaScript/Programmers/level_2/구명보트.js
@@ -1,41 +1,58 @@
-// 2023-02-07
+// 2023-04-14
 function solution(people, limit) {
-  if (people.length === 1) {
-    return 1;
-  }
-
-  const sortedPeople = [...people].sort((a, b) => a - b);
-  let count = 0;
-
-  while (sortedPeople.length) {
-    if (sortedPeople[0] + sortedPeople[sortedPeople.length - 1] <= limit) {
-      sortedPeople.shift();
-    }
-
-    count += 1;
-    sortedPeople.pop();
-  }
-
-  return count;
-}
-
-function solution(people, limit) {
-  const newPeople = [...people].sort((a, b) => a - b);
+  const sortedPeople = people.sort((a, b) => a - b);
+  let result = 0;
   let left = 0;
-  let right = newPeople.length - 1;
-  let totalBoat = 0;
+  let right = people.length - 1;
 
   while (left <= right) {
-    if (newPeople[left] + newPeople[right] > limit) {
-      right -= 1;
-      totalBoat += 1;
-      continue;
-    }
+    if (sortedPeople[left] + sortedPeople[right] <= limit) left += 1;
 
-    left += 1;
+    result += 1;
     right -= 1;
-    totalBoat += 1;
   }
 
-  return totalBoat;
+  return result;
 }
+
+// 2023-02-07
+// function solution(people, limit) {
+//   if (people.length === 1) {
+//     return 1;
+//   }
+
+//   const sortedPeople = [...people].sort((a, b) => a - b);
+//   let count = 0;
+
+//   while (sortedPeople.length) {
+//     if (sortedPeople[0] + sortedPeople[sortedPeople.length - 1] <= limit) {
+//       sortedPeople.shift();
+//     }
+
+//     count += 1;
+//     sortedPeople.pop();
+//   }
+
+//   return count;
+// }
+
+// function solution(people, limit) {
+//   const newPeople = [...people].sort((a, b) => a - b);
+//   let left = 0;
+//   let right = newPeople.length - 1;
+//   let totalBoat = 0;
+
+//   while (left <= right) {
+//     if (newPeople[left] + newPeople[right] > limit) {
+//       right -= 1;
+//       totalBoat += 1;
+//       continue;
+//     }
+
+//     left += 1;
+//     right -= 1;
+//     totalBoat += 1;
+//   }
+
+//   return totalBoat;
+// }
--- practice.js
+++ practice.js
@@ -1,5 +1,7 @@
-for (var a = 1; a < 3; a++) {
-	((a) => setTimeout(() => {
-		console.log(a)
-	}, 1000))(a)
-}
+var a = 1 + 2 + '3';
+var b = 1 + '2' + 3;
+var c = '1' + 2 + 3;
+console.log(a, b, c);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
